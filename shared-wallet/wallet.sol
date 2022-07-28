283afd6e67af967c8910b638156df91cba9e9e59
Hendryboyz <hendry.js1247@gmail.com>
2022-07-28 10:40:56 +0800

    allow to add allowance to someone

--- shared-wallet/wallet.sol
+++ shared-wallet/wallet.sol
@@ -5,11 +5,31 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 
 contract SharedWallet is Ownable {
 
+  mapping(address => uint) public allowance;
+
+  function addAllowance(address _who, uint _amount) public onlyOwner {
+    allowance[_who] += _amount;
+  }
+
+  modifier isOwnerOrAllowed(uint _amount) {
+    require(isOwner() || allowance[msg.sender] >= _amount, "Access denied");
+    _;
+  }
+
   function isOwner() view public returns (bool) {
     return owner() == msg.sender;
   }
 
-  function withdraw(address payable _to, uint _amount) public onlyOwner {
+  function reduceAllowance(address _who, uint _amount) internal isOwnerOrAllowed(_amount) {
+    allowance[_who] -= _amount;
+  }
+
+  function withdraw(address payable _to, uint _amount) public isOwnerOrAllowed(_amount) {
+    bool isFundEnough = _amount <= address(this).balance;
+    require(isFundEnough, "Not enough fund in the contract");
+    if (!isOwner()) {
+      reduceAllowance(msg.sender, _amount);
+    }
     _to.transfer(_amount);
   }
 
